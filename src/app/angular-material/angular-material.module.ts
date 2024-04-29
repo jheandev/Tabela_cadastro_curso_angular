b2aa141bb84739295cb24c4acdbf5a70e4259ea8
jhean carlos <jheanrabelo01@gmail.com>
2024-04-29 10:20:15 -0400

    tela de cadastro finalizada_CURSO_ANGULAR

---
+++ src/app/angular-material/angular-material.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from "@angular/core";
+
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
+
+
+
+
+@NgModule({
+    imports: [
+        MatListModule,
+        MatDividerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatTableModule
+       
+       
+    ],
+    exports: [
+        MatListModule,
+        MatDividerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatTableModule
+     
+    ],
+})
+export class AngularMaterialModule { }
